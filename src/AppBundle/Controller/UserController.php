20e335c89f25f637853e8f922a3261bd4d783bf1
Luis Flores <luispfa@gmail.com>
2018-01-12 12:40:43 +0100

    Edit Record

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -6,6 +6,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Form\FormError;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use AppBundle\Entity\User;
 use AppBundle\Form\UserType;
@@ -26,12 +28,12 @@ class UserController extends Controller {
 
           return new Response($res); */
 
-        $dql = "SELECT u FROM AppBundle:User u";
+        $dql = "SELECT u FROM AppBundle:User u ORDER BY u.id DESC";
         $users = $em->createQuery($dql);
 
         $paginator = $this->get('knp_paginator');
         $pagination = $paginator->paginate($users, $request->query->getInt('page', 1), 3);
-        
+
         return $this->render('user/index.html.twig', array('pagination' => $pagination));
     }
 
@@ -64,20 +66,28 @@ class UserController extends Controller {
 
         if ($form->isValid()) {
             $password = $form->get('password')->getData();
-            $encoder = $this->container->get('security.password_encoder');
-            $encoded = $encoder->encodePassword($user, $password);
-            $user->setPassword($encoded);
 
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($user);
-            $em->flush();
+            $pass_constraint = new Assert\NotBlank();
+            $error_list = $this->get('validator')->validate($password, $pass_constraint);
+            if (count($error_list) == 0) {
+                $encoder = $this->container->get('security.password_encoder');
+                $encoded = $encoder->encodePassword($user, $password);
+                $user->setPassword($encoded);
 
-            $successMsg = $this->get('translator')->trans('The user has been created.');
-            $this->addFlash(
-                    'mensaje', $successMsg
-            );
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
 
-            return $this->redirectToRoute('user_index');
+                $successMsg = $this->get('translator')->trans('The user has been created.');
+                $this->addFlash(
+                        'mensaje', $successMsg
+                );
+
+                return $this->redirectToRoute('user_index');
+            } else {
+                $error_msg = new FormError($error_list[0]->getMessage());
+                $form->get('password')->addError($error_msg);
+            }
         }
 
         // replace this example code with whatever you need
@@ -85,13 +95,79 @@ class UserController extends Controller {
     }
 
     /**
-     * @Route("/user/edit/{id}", name="edit")
+     * @Route("/user/edit/{id}", name="user_edit")
      */
-    public function editAction(Request $request) {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-                    'base_dir' => realpath($this->container->getParameter('kernel.root_dir') . '/..'),
-        ));
+    public function editAction($id) {
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository("AppBundle:User")->find($id);
+
+        if (!$user) {
+            $message = $this->get('translator')->trans('User not found.');
+            throw $this->createNotFoundException($message);
+        }
+
+        $form = $this->createEditForm($user);
+
+        return $this->render('user/edit.html.twig', array('user' => $user, 'form' => $form->createView()));
+    }
+
+    private function createEditForm(User $entity) {
+        $form = $this->createForm(
+                new UserType(), $entity, array('action' => $this->generateUrl('user_update', array('id' => $entity->getId())), 'method' => 'PUT')
+        );
+
+        return $form;
+    }
+
+    /**
+     * @Route("/user/update/{id}", name="user_update")
+     * @Method({"POST", "PUT"})
+     */
+    public function updateAction($id, Request $request) {
+        $em = $this->getDoctrine()->getManager();
+
+        $user = $em->getRepository('AppBundle:User')->find($id);
+
+        if (!$user) {
+            $message = $this->get('translator')->trans('User not found.');
+            throw $this->createNotFoundException($message);
+        }
+
+        $form = $this->createEditForm($user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $form->get('password')->getData();
+
+            if (!empty($password)) {
+                $encoder = $this->container->get('security.password_encoder');
+                $encoded = $encoder->encodePassword($user, $password);
+                $user->setPassword($encoded);
+            } else {
+                $recover_pass = $this->recoverPass($id);
+                $user->setPassword($recover_pass[0]['password']);
+            }
+
+            if ($form->get('role')->getData() == 'ROLE_ADMIN') {
+                $user->setIsActive(1);
+            }
+
+            $em->flush();
+            $successMessage = $this->get('translator')->trans('The user has been modified.');
+            $this->addFlash('mensaje', $successMessage);
+            return $this->redirectToRoute('user_edit', array('id' => $user->getId()));
+        }
+
+        return $this->render('user/edit.html.twig', array('user' => $user, 'form' => $form->createView()));
+    }
+
+    private function recoverPass($id) {
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery('SELECT u.password FROM AppBundle:User u WHERE u.id = :id')
+                ->setParameter('id', $id);
+
+        $current_pass = $query->getResult();
+        return $current_pass;
     }
 
     /**
